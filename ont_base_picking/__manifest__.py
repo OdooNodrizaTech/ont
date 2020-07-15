0d5e0f32e0ef493347bfab5055f15e5ffddad47a
victoralmau <victoralmau@gmail.com>
2020-07-15 07:35:22 +0200

    https://grupoarelux.atlassian.net/browse/ODOOARELUX-951

--- ont_base_picking/__manifest__.py
+++ ont_base_picking/__manifest__.py
@@ -7,7 +7,7 @@
     'website': 'https://nodrizatech.com/',
     'category': 'Tools',
     'license': 'AGPL-3',
-    'depends': ['base', 'purchase', 'sale', 'stock'],
+    'depends': ['base', 'purchase', 'sale', 'stock', 'sale_stock'],
     'data': [
         'views/sale_order.xml',
         'views/stock_picking.xml'
