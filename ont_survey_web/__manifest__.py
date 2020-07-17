5249925e1bd1f7d328aaafd836af5c009729a2aa
victoralmau <victoralmau@gmail.com>
2020-07-17 14:37:00 +0200

    translate

--- ont_survey_web/__manifest__.py
+++ ont_survey_web/__manifest__.py
@@ -1,17 +1,20 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 {
-    'name': 'Ont Survey Web',
-    'version': '12.0.1.0.0',    
-    'author': 'Odoo Nodriza Tech (ONT)',
-    'website': 'https://nodrizatech.com/',
-    'category': 'Tools',
-    'license': 'AGPL-3',
-    'depends': ['base', 'web', 'ont_base_survey'],
-    'data': [
-        'views/template.xml',
+    "name": "Ont Survey Web",
+    "version": "12.0.1.0.0",
+    "author": "Odoo Nodriza Tech (ONT)",
+    "website": "https://nodrizatech.com/",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "web",
+        "ont_base_survey"
     ],
-    'qweb': ['static/src/xml/buttons.xml'],        
-    'installable': True,
-    'auto_install': False,    
+    "data": [
+        "views/template.xml",
+    ],
+    "qweb": ["static/src/xml/buttons.xml"],
+    "installable": True
 }
